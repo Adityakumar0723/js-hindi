03b629bcd7385a50be2e1b3dc3600b782d97470b
Adityakumar0723 <adityamishraself11@gmail.com>
2025-10-24 20:03:00 +0000

    feta type

---
+++ 01_basic/01-dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treate all js code as newer version
+
+alert(3+3) // we are using nodejs, not browser 
+
+
+console.log ("aditya")
+
+let name = "aditya"
+let age = "17"
+let isloggedin = false 
+let state;
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// nul => standard value
+// undefined => value not assigned 
+// symbol => uniqueness use 
+
+// object 
+
+console.log(typeof "aditya"); //  undefined
+console.log(typeof "null"); // object 
+
+
